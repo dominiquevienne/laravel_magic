2cede3fdd0a85146b3730bf9dec281b3f2a95a4c
Dominique Vienne <dominique.vienne@prognolite.ch>
2022-04-13 13:46:57 +0200

    VerifyJwtToken Middleware

---
+++ src/Middleware/VerifyJwtToken.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Dominiquevienne\LaravelMagic\Middleware;
+
+use Dominiquevienne\LaravelMagic\Exceptions\EnvException;
+use Closure;
+use Exception;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+class VerifyJwtToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     * @throws EnvException
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        $jwtToken = $this->getJwtToken($request);
+        $publicKey = env('OAUTH_KEY_PUB');
+
+        if (empty($publicKey)) {
+            throw new EnvException(__('Missing OAUTH_KEY_PUB'));
+        }
+
+        if (empty($jwtToken)) {
+            /**
+             * @todo Use a formatted response
+             */
+            return \response(__('Forbidden'), 403);
+        }
+
+        $visibleLength = 20;
+        $anonymizedToken = substr($jwtToken, 0, $visibleLength) . '...' . substr($jwtToken, strlen($jwtToken) - $visibleLength, $visibleLength);
+
+        try {
+            JWT::decode($jwtToken, new Key($publicKey, 'RS256'));
+        } catch (ExpiredException $e) {
+
+            Log::info(__DIR__ . DIRECTORY_SEPARATOR . __FILE__ . ' - ' . $e->getMessage() . ' - token: ' . $anonymizedToken);
+
+            /**
+             * @todo Use a formatted response
+             */
+            return \response(__('Token expired'), 403);
+        } catch (Exception $e) {
+            /** Log info in logging and not log user in */
+            Log::warning(__DIR__ . DIRECTORY_SEPARATOR . __FILE__ . ' - ' . $e->getMessage() . ' - token: ' . $anonymizedToken);
+
+            /**
+             * @todo Use a formatted response
+             */
+            return \response(__('Invalid token'), 403);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * @param Request $request
+     * @return string|null
+     */
+    private function getJwtToken(Request $request): ?string
+    {
+        $authorizationHeader = $request->header('Authorization');
+
+        return preg_replace('/^Bearer /si', '', $authorizationHeader);
+    }
+}
